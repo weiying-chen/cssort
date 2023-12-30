7d5e89a74fabc70c9ae351d4ed1f0100e744d679
Wei-ying Chen <weiying.alex.chen@gmail.com>
2023-12-30 11:23:49 +0800

    Enable inserting ChatGPT answers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import OpenAI from "openai";
 import dotenv from 'dotenv';
+import readline from 'readline'
 
 dotenv.config();
 
@@ -15,10 +16,22 @@ async function queryChatGPT(prompt) {
           messages: [{"role": "user", "content": prompt }],
         });
 
-        console.log(response.choices[0].message);
+        console.log(response.choices[0].message.content);
     } catch (error) {
         console.error('Error querying ChatGPT:', error);
     }
 }
 
-queryChatGPT("What is dog in Spanish?").then(() => process.exit());
+// queryChatGPT("Log 'Hello World' in JavaScript").then(() => process.exit());
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', (input) => {
+  queryChatGPT(input).then(() => rl.close());
+  // console.log("Thank you for your valuable feedback:", input.toLocaleLowerCase());
+});
+
