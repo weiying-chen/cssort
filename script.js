5fd917191eb9ae44ec8f95945e66f7e722e866b5
Wei-ying Chen <weiying.alex.chen@gmail.com>
2023-12-30 14:20:02 +0800

    Enable modifying content

--- script.js
+++ script.js
@@ -22,16 +22,19 @@ async function queryChatGPT(prompt) {
     }
 }
 
-// queryChatGPT("Log 'Hello World' in JavaScript").then(() => process.exit());
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
-rl.on('line', (input) => {
-  queryChatGPT(input).then(() => rl.close());
-  // console.log("Thank you for your valuable feedback:", input.toLocaleLowerCase());
+const lines = [];
+
+rl.on('line', (line) => {
+    lines.push(line);
 });
 
+rl.on('close', () => {
+    const prompt = lines.join(' ');
+    queryChatGPT(prompt);
+});
