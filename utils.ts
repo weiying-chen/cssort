ffc08bcce7e10690e1778ec91ee463b7d154de00
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-01-27 21:44:32 +0800

    Add objToIndented and getIndentation functions to utils.ts

--- utils.ts
+++ utils.ts
@@ -31,3 +31,26 @@ export function objToCSS(obj: Record<string, string>): string {
 
   return css.trim();
 }
+
+export function objToIndented(
+  obj: Record<string, string | number>,
+  indent = 2,
+) {
+  const indentSpace = ' '.repeat(indent);
+
+  return Object.keys(obj)
+    .map((key) => {
+      const value = typeof obj[key] === 'number' ? obj[key] : `'${obj[key]}'`;
+
+      return `${indentSpace}${key}: ${value}`;
+    })
+    .join(',\n');
+}
+
+export function getIndentation(lines: string[]) {
+  if (lines.length === 0) return 0;
+
+  const leadingWhitespace = lines[0].match(/^\s*/);
+
+  return leadingWhitespace ? leadingWhitespace[0].length : 0;
+}
