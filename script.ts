5e19af6e1fa9dd4ad0a44d0cd90c64154f4d5a63
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-01-28 00:24:48 +0800

    Refactor code to use `inputToObj` function for parsing input

--- script.ts
+++ script.ts
@@ -2,7 +2,7 @@
 import readline from 'readline';
 import postcss from 'postcss';
 import cssDeclarationSorter from 'css-declaration-sorter';
-import { cssToObj, getIndent, objToCSS, objToLines } from './utils';
+import { cssToObj, getIndent, inputToObj, objToCSS, objToLines } from './utils';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -18,8 +18,7 @@ rl.on('line', (line) => {
 
 rl.on('close', async () => {
   const input = lines.join(' ');
-  const indent = getIndent(lines);
-  const obj = new Function(`return { ${input} }`)();
+  const obj = inputToObj(input);
   const css = objToCSS(obj);
 
   try {
@@ -28,9 +27,10 @@ rl.on('close', async () => {
     ]).process(css, { from: undefined });
 
     const obj = cssToObj(result.css);
-    const lines = objToLines(obj, indent);
+    const indent = getIndent(lines);
+    const output = objToLines(obj, indent);
 
-    console.log(lines);
+    console.log(output);
   } catch (error) {
     console.error('PostCSS error:', error);
   }
